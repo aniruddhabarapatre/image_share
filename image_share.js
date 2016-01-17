da7d6cc8f61f7a1a221fc19d64e76dd7cbc72d8e
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2016-01-17 18:58:44 -0500

    Infinite scroll

--- image_share.js
+++ image_share.js
@@ -2,6 +2,24 @@ Images = new Mongo.Collection("images");
 console.log(Images.find().count());
 
 if (Meteor.isClient) {
+  Session.set("imageLimit", 8);
+
+  lastScrollTop = 0;
+  $(window).scroll(function(event){
+    // test if we are near the bottom of the window
+    if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+      // where are we in the page?
+      var scrollTop = $(this).scrollTop();
+      // test if we are going down
+      if (scrollTop > lastScrollTop){
+        // yes we are heading down...
+       Session.set("imageLimit", Session.get("imageLimit") + 4);
+      }
+
+      lastScrollTop = scrollTop;
+    }
+  });
+
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_AND_EMAIL"
   });
