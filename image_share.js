b75ad50dacc653c469968cdf6a4a7e42e5424cf3
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2016-01-17 14:10:32 -0500

    Customise user registration for username

--- image_share.js
+++ image_share.js
@@ -2,6 +2,9 @@ Images = new Mongo.Collection("images");
 console.log(Images.find().count());
 
 if (Meteor.isClient) {
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_AND_EMAIL"
+  });
 
   Template.images.helpers({
     images:Images.find({}, {sort: {createdOn: -1, rating: -1}})
@@ -10,7 +13,7 @@ if (Meteor.isClient) {
   Template.body.helpers({
     username: function () {
       if (Meteor.user()) {
-        return Meteor.user().emails[0].address;
+        return Meteor.user().username;
       } else {
         return "anonymous internet user";
       };
